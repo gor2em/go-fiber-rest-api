859d9b67303e55452e6474b2b59da8cddff7cec0
mekrograca <grkem.squash99@gmail.com>
2023-05-02 16:21:35 +0300

    handler

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,37 @@
 package main
 
-import "go-fiber-rest-api/pkg/config"
+import (
+	"fmt"
+	"go-fiber-rest-api/pkg/config"
+	"go-fiber-rest-api/pkg/user"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	
-	config.LoadEnv()
+	database, err := config.ConnectDB()
+	
+	if err != nil{
+		log.Fatalf("cannot connect to db %v", err)
+	}
 
-	config.ConnectDB()
+	fmt.Println(database.Config)
+
+	repo := user.NewRepository(database)
+	err = repo.Migration()
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	service := user.NewService(repo)
+	handler := user.NewHandler(service)
+
+	app := fiber.New()
+	app.Get("/users/:id", handler.Get)
+	app.Post("/users/", handler.Create)
+
+	app.Listen(":8000")
 
 }
